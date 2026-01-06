b001f9aee153574f96fdbe7b3cbd3e12391abbf8
Catnip56A <maksud.abbaszada@gmail.com>
2026-01-06 16:12:31 +0400

    host update 2

--- app.py
+++ app.py
@@ -21,4 +21,5 @@ def add_cors_headers(response):
     return response
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
+
