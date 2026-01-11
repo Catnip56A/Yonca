16fd5ba131f0350b67ee6ae4dd6ad4a7cb6ca41b
Catnip56A <maksud.abbaszada@gmail.com>
2026-01-11 11:41:54 +0400

    ownership reassign

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -40,6 +40,9 @@ fi
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE yonca_db TO yonca_user;"
 sudo -u postgres psql -d yonca_db -c "GRANT ALL ON SCHEMA public TO yonca_user;"
 
+# Reassign ownership of existing objects to yonca_user
+sudo -u postgres psql -d yonca_db -c "REASSIGN OWNED BY postgres TO yonca_user;"
+
 # Reset migration state if needed
 sudo -u postgres psql -d yonca_db -c "DROP TABLE IF EXISTS alembic_version;"
 
