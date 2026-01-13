f133cfb8f43e204c0ec3c4bfaeba4490d74d81e7
Catnip56A <maksud.abbaszada@gmail.com>
2026-01-13 12:30:15 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
-#from dotenv import load_dotenv
-#import os
+from dotenv import load_dotenv
+import os
 
-#load_dotenv(dotenv_path="/home/magsud/work/Yonca/.env")
+load_dotenv(dotenv_path="/home/magsud/work/Yonca/.env")
 
-#load_dotenv()  # Load environment variables from .env
+load_dotenv()  # Load environment variables from .env
 
 from yonca import create_app
 
