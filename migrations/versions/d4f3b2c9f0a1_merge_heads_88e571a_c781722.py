83212fd6bff3cbd0109d1d103102300455b63dad
Catnip56A <maksud.abbaszada@gmail.com>
2026-01-28 10:43:12 +0400

    Create d4f3b2c9f0a1_merge_heads_88e571a_c781722.py

---
+++ migrations/versions/d4f3b2c9f0a1_merge_heads_88e571a_c781722.py
@@ -0,0 +1,28 @@
+"""Merge heads 88e571a01ac0 and c7817222123b
+
+Revision ID: d4f3b2c9f0a1
+Revises: 88e571a01ac0, c7817222123b
+Create Date: 2026-01-28 11:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd4f3b2c9f0a1'
+down_revision = ('88e571a01ac0', 'c7817222123b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # This is a merge migration; no schema changes required.
+    pass
+
+
+def downgrade():
+    # No downgrade steps for the merge revision.
+    pass
+
+
