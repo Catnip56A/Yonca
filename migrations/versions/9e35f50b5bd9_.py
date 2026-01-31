fa52d7efb867c3d1dbc5435bb7c691e76bee2f81
Catnip56A <maksud.abbaszada@gmail.com>
2026-01-31 17:52:06 +0400

    Create 9e35f50b5bd9_.py

---
+++ migrations/versions/9e35f50b5bd9_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9e35f50b5bd9
+Revises: 
+Create Date: 2026-01-31 17:51:44.188184
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9e35f50b5bd9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
